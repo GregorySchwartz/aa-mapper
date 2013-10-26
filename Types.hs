2b88f1f42cf5762712ad2757566aa88d8c279472
Gregory Schwartz <gs394@drexel.edu>
2013-10-25 23:49:16 -0400

    Types module added.
    All types are now gathered in a single module.

---
+++ Types.hs
@@ -0,0 +1,30 @@
+-- Types module.
+-- By G.W. Schwartz
+--
+-- Collects all application specific types.
+
+module Types where
+
+import qualified Data.Map as M
+
+-- Algebraic
+data GeneticUnit = AminoAcid | Codon
+
+-- Basic
+type AminoAcid         = Char
+type Codon             = String
+type Sequence a        = [a]
+type ID                = Int
+type Clone a           = Sequence a
+type Germline a        = Sequence a
+type Position          = Int
+type Diversity         = Int
+type Size              = Int
+
+-- Advanced
+type Mutation a    = (a, a)
+type CloneMap a    = M.Map (ID, Germline a) [Clone a]
+type MutationMap a = M.Map Position [Mutation a]
+type CloneMutMap a = M.Map (ID, Germline a) (MutationMap a)
+type DiversityMap  = M.Map Position Diversity
+type ChangedAAMap  = M.Map Diversity [[(AminoAcid, AminoAcid, Size)]]
